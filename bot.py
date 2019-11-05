9b613a1887bcfd5b5c29d1dfa4d962bbe4bb2043
r1chter1 <bolatov.2003@inbox.ru>
2019-11-05 05:34:43 +0500

    the method deleting the msges

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import constants
 import time
 from datetime import datetime
 import sql_queries as sql
+import logging
+from time import sleep
 
 updater = Updater(token=constants.toqen)
 dispatcher = updater.dispatcher
@@ -12,10 +15,42 @@ dispatcher = updater.dispatcher
 
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Hey I was created by r1chter1.')
+    help_bot(bot, update)
 
 def textMessage(bot, update):
     response = 'MAL'
     bot.send_message(chat_id=update.message.chat_id, text=response)
+
+logger = logging.getLogger()
+
+def draft(bot, update):
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                 InlineKeyboardButton("Option 2", callback_data='2')],
+                [InlineKeyboardButton("Option 3", callback_data='3'),
+                InlineKeyboardButton("NIS-web", url='https://i0bster.github.io/nis-web/')]]
+    
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+def help_bot(bot, update):
+    response = ['/timetable "class"', '/help - to see commands', '/del - to clear chat']
+    for i in response:
+        bot.send_message(chat_id=update.message.chat_id, text=i)
+    
+
+def delete_msg(bot, update):
+    print(update.message.message_id, type(update.message.message_id))
+    bot.send_message(chat_id=update.message.chat_id, text='Cleaning in process...')
+    lastid = update.message.message_id + 1
+    print('qq', lastid, type(lastid))
+    sleep(2)
+    for i in range(1000):
+        msgid = update.message.message_id - i
+        bot.deleteMessage(chat_id=update.message.chat_id, message_id=msgid)    
+    bot.deleteMessage(chat_id=update.message.chat_id, message_id=lastid)
+
+
+
 
 def individualreq(bot, update, args):
     args[-1] = args[-1].upper()
@@ -32,7 +67,7 @@ def individualreq(bot, update, args):
     elif datetime.today().weekday() == 1:
         res = sql.tuesday(w)
         print(res)
-        for lesson in res[1]:
+        for lesson in res[0]:
             bot.send_message(chat_id=update.message.chat_id, text=lesson)
 
     elif datetime.today().weekday() == 2:
@@ -61,9 +96,15 @@ def individualreq(bot, update, args):
 
 start_command_handler = CommandHandler(['start'], startCommand, pass_args=False)
 text_message_handler = MessageHandler(Filters.text, textMessage)
+draft = CommandHandler(['draft'], draft, pass_args=False)
+help_bot_handler = CommandHandler(['help'], help_bot, pass_args=False)
+delete_bot_handler =CommandHandler(['del'], delete_msg, pass_args=False)
 
+dispatcher.add_handler(help_bot_handler)
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
+dispatcher.add_handler(draft)
+dispatcher.add_handler(delete_bot_handler)
 dispatcher.add_handler(CommandHandler(['timetable'], individualreq, pass_args=True))
 
 updater.start_polling(clean=True)
--- constants.py
+++ constants.py
@@ -1,2 +1,12 @@
-toqen = 'your-token' #input your generated token
+import requests as req
+import logging
+
+logger = logging.getLogger()
+
+toqen = '659340911:AAFdMGvuQKz6r933tLhne-w8clab8QWifZo'
 password = 'qwert13'
+
+def fetch_json(base, route):
+    product = req.get(base + route)
+    logger.info("a request to the api for {route} was sent")
+    return product.json()
